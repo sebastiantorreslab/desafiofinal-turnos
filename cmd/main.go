75f8bd5cce29daeca9b613e6b21061b6dcfd3b8e
sebastiantorreslab <str9371@gmail.com>
2024-03-27 02:36:43 -0500

    main actualixed

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	handlerDentist "github.com/sebastiantorreslab/desafiofinal-turnos/cmd/server/handler/dentist"
+	handlerShift "github.com/sebastiantorreslab/desafiofinal-turnos/cmd/server/handler/shift"
 	handlerPatient "github.com/sebastiantorreslab/desafiofinal-turnos/cmd/server/handler/patient"
 	"github.com/sebastiantorreslab/desafiofinal-turnos/internal/dentist"
 	"github.com/sebastiantorreslab/desafiofinal-turnos/internal/patient"
+	"github.com/sebastiantorreslab/desafiofinal-turnos/internal/shift"
+
 	"github.com/sebastiantorreslab/desafiofinal-turnos/pkg/store"
 )
 
@@ -21,15 +24,20 @@ func main() {
 
 	defer db.Close()
 
-	storage := store.NewSqlDentistStore(db)
-	repository := dentist.NewRepository(storage)
-	service := dentist.NewService(repository)
-	dentistHandler := handlerDentist.NewDentistHandler(service)
+	storageDentist := store.NewSqlDentistStore(db)
+	repositoryDentist := dentist.NewDentistRepository(storageDentist)
+	serviceDentist := dentist.NewDentistService(repositoryDentist)
+	dentistHandler := handlerDentist.NewDentistHandler(serviceDentist)
 
 	storagePatient := store.NewSqlStorePatient(db)
 	repositoryPatient := patient.NewPatientRepository(storagePatient)
 	servicePatient := patient.NewService(repositoryPatient)
 	patientHandler := handlerPatient.NewPatientHandler(servicePatient)
+
+	storageShift := store.NewSqlShiftStore(db)
+	repositoryShift := shift.NewShiftRepository(storageShift)
+	serviceShift := shift.NewShiftService(repositoryShift)
+	shiftHandler := handlerShift.NewShiftHandler(serviceShift)
 
 	server := gin.Default()
 
@@ -44,7 +52,14 @@ func main() {
 	dentists.PATCH(":id", dentistHandler.UpdateByField())
 	dentists.DELETE(":id", dentistHandler.Delete())
 
-	}
+	shifts := server.Group("/shifts")
+
+	shifts.GET("", shiftHandler.GetAll())
+	shifts.GET(":id", shiftHandler.GetById())
+	shifts.POST("", shiftHandler.Create())
+	shifts.PUT(":id", shiftHandler.Update())
+	shifts.PATCH(":id", shiftHandler.UpdateByField())
+	shifts.DELETE(":id", shiftHandler.Delete())
 
 	// Rutas para pacientes
 	patients := server.Group("/patients")
