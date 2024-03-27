831d33bb9369bff8969e7fe1dacc99c08e98f1d6
sebastiantorreslab <str9371@gmail.com>
2024-03-27 00:14:07 -0500

    storage for shift in progress

--- pkg/store/shiftSql.go
+++ pkg/store/shiftSql.go
@@ -19,28 +19,104 @@ func NewSqlShiftStore(db *sql.DB) StoreShiftInterface {
 
 func (s *sqlShiftStore) GetById(id int) (domain.Shift, error) {
 
-	
+	var shift domain.Shift
+	query := "SELECT * FROM shift WHERE id=?;"
 
+	stmt := s.db.QueryRow(query, id)
 
-
-
+	err := stmt.Scan(&shift.ID, &shift.Patient, &shift.Dentist, &shift.ShiftHour, &shift.ShiftDate)
+	if err != nil {
+		return domain.Shift{}, err
+	}
 
 	return domain.Shift{}, nil
 
 }
 func (s *sqlShiftStore) GetAll() ([]domain.Shift, error) {
-	return []domain.Shift{}, nil
+
+	query := "SELECT * FROM `clinic-db`.shift"
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var shifts []domain.Shift
+
+	for rows.Next() {
+		var shift domain.Shift
+		err := rows.Scan(&shift.ID, &shift.Patient, &shift.Dentist, &shift.ShiftHour, &shift.ShiftDate)
+		if err != nil {
+			return nil, err
+
+		}
+		shifts = append(shifts, shift)
+	}
+	if err := rows.Err(); err != nil {
+		return []domain.Shift{}, err
+	}
+
+	return shifts, nil
 
 }
 func (s *sqlShiftStore) Update(shift domain.Shift, id int) error {
+
+	query := "UPDATE shift SET shift_date = ?, shift_hour = ?, id_patient=?,id_dentist=? WHERE id=?;"
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	res, err := stmt.Exec(shift.ShiftDate, shift.ShiftHour, shift.Patient.ID, shift.Dentist.ID, id)
+	if err != nil {
+		return err
+	}
+
+	_, err = res.RowsAffected()
+	if err != nil {
+		return err
+	}
 	return nil
 
 }
 func (s *sqlShiftStore) Delete(id int) error {
+
+	query := "DELETE FROM shift WHERE id = ?;"
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = res.RowsAffected()
+	if err != nil {
+		return err
+	}
 	return nil
 
 }
 func (s *sqlShiftStore) Create(shift domain.Shift) (domain.Shift, error) {
-	return domain.Shift{}, nil
+
+	query := "INSERT INTO shift (shift_date, shift_hour, id_patient,id_dentist) VALUES (?, ?, ?,?);"
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return domain.Shift{}, err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(shift.ShiftDate, shift.ShiftHour, shift.Patient.ID, shift.Dentist.ID)
+	if err != nil {
+		return domain.Shift{}, err
+	}
+	_, err = res.RowsAffected()
+	if err != nil {
+		return domain.Shift{}, err
+	}
+
+	return shift, nil
 
 }
