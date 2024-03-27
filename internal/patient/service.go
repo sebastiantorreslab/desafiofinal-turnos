359d17b566e6df1f517723550578d61485ce12d1
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2024-03-27 00:03:09 -0500

    corrections patient service

--- internal/patient/service.go
+++ internal/patient/service.go
@@ -17,7 +17,7 @@ type patientService struct {
 	r IPatientRepository
 }
 
-func NewService(r IPatientRepository) IPatientRepository {
+func NewService(r IPatientRepository) IPatientService {
 	return &patientService{r}
 }
 
@@ -25,7 +25,7 @@ func (s *patientService) Create(patient domain.Patient) (domain.Patient, error) 
 
 	d, err := s.r.Create(patient)
 	if err != nil {
-		return domain.Patient{}, nil
+		return domain.Patient{}, err
 	}
 
 	return d, nil
@@ -52,10 +52,41 @@ func (s *patientService) GetById(id int) (domain.Patient, error) {
 
 func (s *patientService) Update(patient domain.Patient, id int) (domain.Patient, error) {
 
-	d, err := s.r.GetById(id)
+	currentPatient, err := s.r.GetById(id)
 	if err != nil {
 		return domain.Patient{}, err
 	}
+
+	if patient.DNI != 0 {
+		currentPatient.DNI = patient.DNI
+	}
+	if patient.Name != "" {
+		currentPatient.Name = patient.Name
+	}
+	if patient.LastName != "" {
+		currentPatient.LastName = patient.LastName
+	}
+	if patient.Address != "" {
+		currentPatient.Address = patient.Address
+	}
+	if !patient.AdmissionDate.IsZero() {
+		currentPatient.AdmissionDate = patient.AdmissionDate
+	}
+
+	err = s.r.Update(currentPatient, id)
+	if err != nil {
+		return domain.Patient{}, err
+	}
+
+	return currentPatient, nil
+}
+
+func (s *patientService) UpdateByField(patient domain.Patient, id int) error {
+	d, err := s.r.GetById(id)
+	if err != nil {
+		return err
+	}
+
 	if patient.DNI != 0 {
 		d.DNI = patient.DNI
 	}
@@ -74,9 +105,19 @@ func (s *patientService) Update(patient domain.Patient, id int) (domain.Patient,
 
 	err = s.r.Update(d, id)
 	if err != nil {
-		return domain.Patient{}, err
+		return err
 	}
 
-	return d, nil
+	return nil
+}
+
+func (s *patientService) Delete(id int) error {
+
+	err := s.r.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
