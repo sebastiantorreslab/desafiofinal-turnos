37359272022503816c8431bec6f638c8c6db212e
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2024-03-26 23:51:23 -0500

    implementation function update patient service

--- internal/patient/service.go
+++ internal/patient/service.go
@@ -49,3 +49,34 @@ func (s *patientService) GetById(id int) (domain.Patient, error) {
 	}
 	return d, nil
 }
+
+func (s *patientService) Update(patient domain.Patient, id int) (domain.Patient, error) {
+
+	d, err := s.r.GetById(id)
+	if err != nil {
+		return domain.Patient{}, err
+	}
+	if patient.DNI != 0 {
+		d.DNI = patient.DNI
+	}
+	if patient.Name != "" {
+		d.Name = patient.Name
+	}
+	if patient.LastName != "" {
+		d.LastName = patient.LastName
+	}
+	if patient.Address != "" {
+		d.Address = patient.Address
+	}
+	if !patient.AdmissionDate.IsZero() {
+		d.AdmissionDate = patient.AdmissionDate
+	}
+
+	err = s.r.Update(d, id)
+	if err != nil {
+		return domain.Patient{}, err
+	}
+
+	return d, nil
+
+}
